3e0c31addfd95e52a5c2e28e30f80040b32fa4ce
Ilnur_Ishkaev <ilnurishkaev@yandex.ru>
2020-07-09 03:10:49 +0300

    Funny Geometry to github.

---
+++ Assets/Scripts/BuildMesh.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+
+public class BuildMesh : MonoBehaviour 
+{
+	// for flexibility of constructing different meshes (not only pyramid mesh)
+	// use cube mesh template and assign its top vertices 
+	public Vector3 vertLeftTopFront  = new Vector3(-1,1,1);
+	public Vector3 vertRightTopFront = new Vector3(1,1,1);
+	public Vector3 vertRightTopBack  = new Vector3(1,1,-1);
+	public Vector3 vertLeftTopBack   = new Vector3(-1,1,-1);
+
+	// Use this for initialization
+	void Start () 
+	{
+		MeshFilter meshFilter = GetComponent<MeshFilter>();
+		Mesh mesh = meshFilter.mesh;
+
+		// vertices
+		Vector3[] vertices = new Vector3[]
+		{
+			// front 
+			vertLeftTopFront,     // 0
+			vertRightTopFront,    // 1
+			new Vector3(-1,-1,1), // 2
+			new Vector3(1,-1,1),  // 3
+
+			// back 
+			vertRightTopBack,     // 4
+			vertLeftTopBack,      // 5
+			new Vector3(1,-1,-1), // 6
+			new Vector3(-1,-1,-1),// 7
+
+			// left
+			vertLeftTopBack,      // 8
+			vertLeftTopFront,     // 9
+			new Vector3(-1,-1,-1),// 10
+			new Vector3(-1,-1,1), // 11
+
+			// right
+			vertRightTopFront,    // 12
+			vertRightTopBack,     // 13
+			new Vector3(1,-1,1),  // 14
+			new Vector3(1,-1,-1), // 15
+
+			// top
+			vertLeftTopBack,      // 16
+			vertRightTopBack,     // 17
+			vertLeftTopFront,     // 18
+			vertRightTopFront,    // 19
+
+			// bottom
+			new Vector3(-1,-1,1), // 20
+			new Vector3(1,-1,1),  // 21
+			new Vector3(-1,-1,-1),// 22
+			new Vector3(1,-1,-1)  // 23
+
+		};
+
+		// triangles
+		int[] triangles = new int[]
+		{
+			// front 
+			0,2,3,
+			3,1,0,
+
+			// back 
+			4,6,7,
+			7,5,4,
+
+			// left 
+			8,10,11,
+			11,9,8,
+
+			// right
+			12,14,15,
+			15,13,12,
+
+			// top 
+			16,18,19,
+			19,17,16,
+
+			// bottom 
+			20,22,23,
+			23,21,20
+		};
+
+		// uvs
+		Vector2[] uvs = new Vector2[]
+		{
+			new Vector2(0,1),
+			new Vector2(0,0),
+			new Vector2(1,1),
+			new Vector2(1,0),
+
+			new Vector2(0,1),
+			new Vector2(0,0),
+			new Vector2(1,1),
+			new Vector2(1,0),
+
+			new Vector2(0,1),
+			new Vector2(0,0),
+			new Vector2(1,1),
+			new Vector2(1,0),
+
+			new Vector2(0,1),
+			new Vector2(0,0),
+			new Vector2(1,1),
+			new Vector2(1,0),
+
+			new Vector2(0,1),
+			new Vector2(0,0),
+			new Vector2(1,1),
+			new Vector2(1,0),
+
+			new Vector2(0,1),
+			new Vector2(0,0),
+			new Vector2(1,1),
+			new Vector2(1,0)
+		};
+
+		mesh.Clear ();
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.uv = uvs;
+		mesh.Optimize();
+		mesh.RecalculateNormals();
+	
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/GetColorFromCamera.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using easyar;
+
+public class GetColorFromCamera : MonoBehaviour
+{
+    public CameraImageRenderer cameraRenderer;
+    private Material _material; 
+    public Color MaterialColor { get { return _material.color; } private set { _material.color = value; } }
+    private ImageTargetController _imageTarget;
+    private RenderTexture _renderTexture;
+    private Texture2D _cameraTexture;
+
+    [Range(1,5)] [SerializeField] float _scaleCameraCoefficient = 1f;
+    public Vector2[] relativeTargetAnglePoints;
+    private Vector3[] _targets;
+    [SerializeField] int _calculatingAreaSize = 1;
+
+    [SerializeField] int _textFont = 1;
+
+    private void Awake()
+    {
+        _imageTarget = GetComponentInParent<ImageTargetController>();
+        _material = GetComponent<MeshRenderer>().material;
+        cameraRenderer.RequestTargetTexture((camera, texture) => { _renderTexture = texture; });    
+        _cameraTexture = new Texture2D(0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // set target points
+        _targets = SetTargetPoints();
+
+        // copy texture from RenderTexture to Texture2D
+        if (_cameraTexture.width == 0 && _cameraTexture.height == 0) 
+            _cameraTexture.Resize(_renderTexture.width, _renderTexture.height); 
+
+        StartCoroutine(GetTextureFromRender());
+
+        // check if the object is within the bounds and assign new color
+        if (IsWithinBounds())
+        {
+            AssignNewColorToObject();
+        }
+
+    }
+
+    private Vector3[] SetTargetPoints()
+    {
+        Vector3[] targets = new Vector3[relativeTargetAnglePoints.Length];
+
+        // local coordinates of image points (see below)
+        var halfWidth = 0.5f/_scaleCameraCoefficient;
+        var halfHeight = halfWidth / _imageTarget.Target.aspectRatio();
+
+        for (int i = 0; i < relativeTargetAnglePoints.Length; i++)
+        {
+            float x = relativeTargetAnglePoints[i].x;
+            float y = relativeTargetAnglePoints[i].y;
+            Vector3 targetAnglePoint = _imageTarget.transform.TransformPoint(new Vector3( x * halfWidth,  y * halfHeight, 0));
+            targets[i] = Camera.main.WorldToScreenPoint(targetAnglePoint);
+        }
+
+        return targets;
+    }
+
+    private bool IsWithinBounds()
+    {
+        int numberOfItemInBounds = 0;
+
+        if (_targets != null)
+        {
+            foreach (Vector3 item in _targets)
+            {
+                numberOfItemInBounds += Convert.ToInt32(IsTargetInBounds(item, _calculatingAreaSize, _cameraTexture));
+            }
+        }
+
+        return numberOfItemInBounds == _targets?.Length;        
+    }
+
+    private void AssignNewColorToObject()
+    {
+        Color[][] quads = new Color[_targets.Length][];
+        Color quadAvg = new Color(0f,0f,0f,0f);
+
+        // get pixels from image areas and calculate average color
+        for (int i = 0; i < _targets.Length; i++)
+        {
+            quads[i] = _cameraTexture.GetPixels((int)_targets[i].x - _calculatingAreaSize, (int)_targets[i].y - _calculatingAreaSize, _calculatingAreaSize, _calculatingAreaSize);
+            quadAvg += GetAverageColor(quads[i]);
+        }
+        quadAvg /= quads.Length;
+        
+        // if recent object material color is "greater" than calculating average, then assign new material color
+        if (isColorGreaterThan(MaterialColor, quadAvg))
+            MaterialColor = quadAvg;
+    }
+
+    private IEnumerator GetTextureFromRender()
+    {
+        yield return new WaitForEndOfFrame();
+        RenderTexture.active = _renderTexture;
+        _cameraTexture.ReadPixels(new Rect(0, 0, _cameraTexture.width, _cameraTexture.height), 0, 0);
+        _cameraTexture.Apply();
+    }
+
+    private bool isColorGreaterThan(Color lhs, Color rhs)
+    {
+        return lhs.a > rhs.a || lhs.r > rhs.r ||
+               lhs.g > rhs.g || lhs.b > rhs.b;
+    }
+    
+    private bool IsTargetInBounds(Vector3 point, int calculatingAreaSize, Texture2D texture)
+    {
+        return point.x - calculatingAreaSize > 0 && point.x + calculatingAreaSize < texture.width  &&
+               point.y - calculatingAreaSize > 0 && point.y + calculatingAreaSize < texture.height;
+    }
+
+    private Color GetAverageColor(Color[] colors)
+    {
+        Color avg = new Color(0,0,0,0);
+        foreach (var color in colors) {
+            avg += color;
+        }
+        avg /= colors.Length;
+        return avg;
+    }
+
+    void OnValidate()
+    {
+        foreach (Vector2 item in relativeTargetAnglePoints)
+        {
+            if (item.x > 1 || item.y > 1 || item.x < -1 || item.y < -1)
+            {   
+                throw new Exception ("Relative point vector items must be in range [-1;1]!");
+            }   
+                
+        }
+    }
+    
+    void OnGUI()
+    {
+        if (!IsWithinBounds()) {
+            GUIStyle style = new GUIStyle ();
+            style.fontSize = (int)(_textFont * _cameraTexture.width / 1000);
+            style.alignment = TextAnchor.MiddleCenter;
+            GUI.Label(new Rect(_cameraTexture.width/2-_cameraTexture.width/8, _cameraTexture.height/2-_cameraTexture.width/8, 
+                               _cameraTexture.width/4, _cameraTexture.width/4), 
+                               "QR marker is out of bounds\nColor may be incorrect", style);
+        }
+
+    }
+
+}
